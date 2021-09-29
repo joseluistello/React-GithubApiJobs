fd117b3f2c0f5782806d0c26ef0e47a391b0d725
José Luis Tello <jltello96@gmail.com>
2021-09-28 23:45:42 -0500

    Adds

---
+++ src/useFetchJobs.js
@@ -0,0 +1,19 @@
+import { useReducer } from 'react'
+
+function reducer(state, action) {
+
+}
+
+export default function useFetchJobs(params, page) {
+    const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true})
+    
+    
+    
+    
+    return {
+        jobs: [],
+        loading: false,
+        error: false
+    }
+
+}
